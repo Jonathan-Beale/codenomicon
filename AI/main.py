06b84ca5c4c9152b9a54f3899c622d1bcc236e48
PandaSmasher <jonathanmk.beale@gmail.com>
2023-10-18 14:56:46 -0400

    Made some improvements, read README

--- AI/main.py
+++ AI/main.py
@@ -2,6 +2,8 @@ import os
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
+MAX_SCOPE_LEN = 5000
+
 key = input("Open AI Key: ")
 
 print("Loading Models...")
@@ -10,20 +12,41 @@ print("Loaded.")
 
 def get_response(file_content, chat_history, user_input, system_prompt):
     messages = []
-    messages.append(SystemMessage(content = file_content))
     for turn in chat_history:
         messages.append(HumanMessage(content=turn.query))
         messages.append(AIMessage(content=turn.response))
     messages.append(SystemMessage(content=system_prompt))
+    messages.append(HumanMessage(content = "Here is my code:" + file_content))
     messages.append(HumanMessage(content=user_input))
+
+    # FOR DEBUG
+    # contents = []
+    # for message in messages:
+    #     contents.append(message.content)
+    # return contents
     return remus(messages=messages).content
 
 def replaced(response):
     messages = []
     messages.append(AIMessage(content=response))
-    messages.append(HumanMessage(content="What code would we replace?"))
+    messages.append(HumanMessage(content="What lines of code does the new code replace?"))
     return remus(messages=messages).content
 
+def get_scope(file_paths):
+    """
+    Takes a list of file paths and iterates through them adding the file contents to a string.
+    Returns a string of each file formatted in markdown, stops at context length cap.
+    """
+    scope_content = ""
+    for path in file_paths:
+        with open(path, 'r') as file:
+            content = file.read()
+            if content.length + scope_content.length < MAX_SCOPE_LEN:
+                scope_content.append("```python\n" + content + "```\n")
+            else:
+                scope_content.append("OUT OF CONTEXT LENGTH\n")
+                return scope_content
+    return scope_content
 
 def get_content(file_path):
     try:
@@ -36,15 +59,16 @@ def get_content(file_path):
         return f"Error: {str(e)}"
 
 
-code_path = "example.py"
-prompt_path = "prompt.txt"
+code_path = "C:/Users/Jonathan/codenomicon/AI/example.py"
+user_path = "C:/Users/Jonathan/codenomicon/AI/user_in.txt"
+prompt_path = "C:/Users/Jonathan/codenomicon/AI/prompt.txt"
 
-running = True
-while running:
-    user_in = input("USER> ")
+while input("USER> ") == "run":
     solution = get_response(file_content=("```\n" + get_content(code_path) + "\n```"),
                  chat_history=[],
-                 user_input=user_in,
+                 user_input=get_content(user_path),
                  system_prompt=get_content(prompt_path))
     print(solution)
-    print(replaced(solution))
+    # print(replaced(solution))
+
+# print(get_content(code_path), get_content("C:\\Users\\Jonathan\\codenomicon\\AI\\user_in.txt"), get_content(prompt_path))
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # codenomicon
+
+## AI
+The heart of the application's purpose is the AI. The AI will start out as an assistant instructed to print out a complete code snippet to replace the old code with. We will then detect the differences between the new code and the old code with a simple algorithm, and provide the users an easy button to make the changes. Eventually, the goal is to have an option to automate the AI troubleshooting code by having it loop: [run] > [debug] > [run] > [debug]
+
+### Prompting
+The AI is provided a style guide on responses to make sure it gives context from the code, allowing us to run the similarity algorithm.
+The AI is also fed the last few messages in its chat history, and the content of each file. Eventually we would like it to automatically recieve terminal output as well. This would allow for the auto debugging.
